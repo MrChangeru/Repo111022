b92edce8f5781f8d76d03db6401d263d999851ed
Студент KSI <student@ksi.esstu.ru>
2022-10-11 16:34:47 +0800

    create new repository

---
+++ Work12/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Work12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
