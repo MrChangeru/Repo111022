3f37a162965ccd102e328160f345c69d819aff15
Студент KSI <student@ksi.esstu.ru>
2022-10-11 17:24:33 +0800

    change file Program.cs

--- Work12/Program.cs
+++ Work12/Program.cs
@@ -6,7 +6,15 @@ namespace Work12
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Введите число -> ");
+            int x;
+            x = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            do{
+                sum += x % 10;
+                x /= 10;                      
+            }while(x!=0);
+            Console.WriteLine("Сумма = {0}",sum);
         }
     }
 }
